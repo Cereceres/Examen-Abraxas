2a949fc2464fec05c040367bf001858bb6b0d098
cereceres <jesus.edelcereceres@gmail.com>
2017-07-19 15:28:37 -0500

    the test are ready

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Examen-Abraxas
 El examen a realizar para abraxas
+
+# Requiriments
+
+Install nodejs >= 6
+# Init
+
+```bash
+ npm install
+```
+
+# Run test
+
+```bash
+ npm start -- path-to-folder http://user:pass@hostofNeo4j:7474 
+```
+
+# Spedifications
+
+Only read the sql files extensions and the format in test.sql
--- test.sql
+++ test.sql
@@ -1,11 +1,11 @@
 use test2
-select first_name2, last_name2, order_date2, order_amount2
+select customers2.first_name2, customers2.last_name2, orders2.order_date2, orders2.order_amount2
 from customers2 c
 right join orders2 o
 on c.customer_id = o.customer_id
 where first_name is hola
 use test
-select first_name, last_name, order_date, order_amount
+select first_name, last_name, orders.order_date, orders.order_amount
 from customers c
 right join orders o
 on c.customer_id = o.customer_id
